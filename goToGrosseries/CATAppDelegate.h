e9b19109c8bf032ddf0dc7131dd5a4ac2d0c64b6
Pedro Catoira <pedro.catoira@gmail.com>
2014-02-06 22:34:38 -0800

    Initial Commit

---
+++ goToGrosseries/CATAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CATAppDelegate.h
+//  goToGrosseries
+//
+//  Created by Pedro Catoira on 14-02-06.
+//  Copyright (c) 2014 Pedro Catoira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CATAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ goToGrosseries/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  goToGrosseries
+//
+//  Created by Pedro Catoira on 14-02-06.
+//  Copyright (c) 2014 Pedro Catoira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CATAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CATAppDelegate class]));
+    }
+}
